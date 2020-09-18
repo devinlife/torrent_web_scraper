de25f935231d60be8e2dfb2c4a7c1102ac1d54ae
devinlife <devinlifeidea@gmail.com>
2020-09-19 00:24:18 +0900

    초기 설정 및 실행 스크립트 정비

--- execute_scrapers.sh
+++ execute_scrapers.sh
@@ -1,1 +1,4 @@
-env/bin/python torrent_web_scraper.py
+#!/bin/bash
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+$SCRIPT_PATH/env/bin/python torrent_web_scraper.py
--- setup_script.sh
+++ setup_script.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+which virtualenv
+RET=`echo $?`
+
+echo $RET
+if [[ $RET -gt 0 ]]
+then
+  sudo pip3 install virtualenv
+fi
+
 virtualenv --python=python3.7 env
 env/bin/pip install -r requirements.txt
 cp local_config/local_machine_configuration_sample.json local_config/local_machine_configuration.json
