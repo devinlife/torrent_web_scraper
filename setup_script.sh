b8b61111e4b93d9e9430cf8440b4f11ad5bff6b8
devinlife <devinlifeidea@gmail.com>
2020-09-20 21:24:10 +0900

    program list 파일도 로컬 설정으로 등록

--- setup_script.sh
+++ setup_script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 which virtualenv
 RET=`echo $?`
-
-echo $RET
-if [[ $RET -gt 0 ]]
+echo virtualenv installed, $RET
+if [ "$RET" = "1" ]
 then
   sudo pip3 install virtualenv
 fi
@@ -11,3 +10,4 @@ fi
 virtualenv --python=python3.7 env
 env/bin/pip install -r requirements.txt
 cp local_config/local_machine_configuration_sample.json local_config/local_machine_configuration.json
+cp local_config/program_list_sample.py local_config/program_list.py
