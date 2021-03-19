e7ddbc4cda97ef0dffb2ccc382365499cbd4f2ab
yeonminpark <yeonminpark@gmail.com>
2021-03-19 17:17:18 +0900

    Debugging

--- setup_script.sh
+++ setup_script.sh
@@ -10,4 +10,3 @@ fi
 virtualenv --python=python3.7 env
 env/bin/pip install -r requirements.txt
 cp local_config/local_machine_configuration_sample.json local_config/local_machine_configuration.json
-cp local_config/program_list_sample.py local_config/program_list.py
